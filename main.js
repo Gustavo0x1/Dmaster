987c297f7e0a6f3b42984ffca1cb935a4dab92a8
Gustavo Soares <gsr120104@gmail.com>
2025-05-12 20:28:46 -0300

    Dmaster
    Setting Up

---
+++ main.js
@@ -0,0 +1,19 @@
+const {app,BrowserWindow} = require('electron');
+const url = require('url')
+const path = require('path')
+function createMainWindow(){
+
+    const MainWindow = new BrowserWindow({
+        title:'elc',
+        width:'1280',
+        height:'720'
+    })
+    const startUrl = url.format({
+        pathname:path.join(__dirname,'index.html'),
+        protocol:'file'
+    })
+    MainWindow.loadURL(startUrl)
+}
+
+
+app.whenReady().then(createMainWindow)
